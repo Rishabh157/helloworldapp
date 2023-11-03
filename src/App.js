9f183d5b915460d411e781b3d738dd8295e8e809
Rishabh157 <rishabhgour157@gmail.com>
2023-11-03 17:44:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  function applyOfOpration(arr, opration) {
+
+    const result = [];
+
+    for (let ele of arr) {
+      result.push(opration(ele))
+    }
+
+    return result
+
+  }
+
+  const squareArr = applyOfOpration([1, 2, 3, 4, 5], function (x) {
+    return x * x
+  })
+
+
+  console.log(squareArr)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO WORLD</h1>
     </div>
   );
 }
